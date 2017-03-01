5c89d317011368bbd113a6ead12920abe32d2031
Liang Hong <lianghongbuaa@gmail.com>
2017-03-01 20:43:03 +0800

    add lambda version of partitionPrimes

--- src/main/java/java8inaction/stream/collector/CollectorHarness.java
+++ src/main/java/java8inaction/stream/collector/CollectorHarness.java
@@ -1,5 +1,13 @@
 package java8inaction.stream.collector;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+import static java8inaction.stream.collector.PrimeNumbersCollector.isPrime;
+
 public class CollectorHarness {
     public static void main(String[] args) {
         long fastest = Long.MAX_VALUE;
@@ -13,5 +21,22 @@ public class CollectorHarness {
             }
             System.out.println("Faster execution done in " + fastest + " msecs");
         }
+    }
+
+    public Map<Boolean, List<Integer>> partitionPrimesWithCustomCollector(int n) {
+        return IntStream.rangeClosed(2, n).boxed().collect(
+                () -> new HashMap<Boolean, List<Integer>>() {{
+                    put(true, new ArrayList<Integer>());
+                    put(false, new ArrayList<Integer>());
+                }},
+                (acc, candidate) -> {
+                    acc.get(isPrime(acc.get(true), candidate)).add(candidate);
+
+                },
+                (map1, map2) -> {
+                    map1.get(true).addAll(map2.get(true));
+                    map1.get(false).addAll(map2.get(false));
+                }
+        );
     }
 }
