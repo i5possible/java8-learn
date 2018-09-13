9a218fa474d712f0cd327fbf3775ad4bad3b58c6
Liang Hong <lianghongbuaa@gmail.com>
2018-09-13 13:56:42 +0800

    add consumerTest

--- src/main/java/java8inaction/lambda/ConsumerTest.java
+++ src/main/java/java8inaction/lambda/ConsumerTest.java
@@ -1,13 +1,28 @@
 package java8inaction.lambda;
 
 import java.util.function.Consumer;
-import java.util.function.DoubleConsumer;
-import java.util.function.IntConsumer;
 
 public class ConsumerTest {
+
+    private final static String name = "Ma XiaoQiang";
+
     public static void main(String[] args) {
         Consumer consumer = System.out::println;
-        IntConsumer intConsumer;
-        DoubleConsumer doubleConsumer;
+
+        Consumer printTwice = (name) -> {
+            System.out.println("1: " + name);
+            System.out.println("2: " + name);
+        };
+
+        Consumer<String> ddd = (String adf) -> {
+
+        };
+
+        handleEvent(consumer);
+        handleEvent(printTwice);
+    }
+
+    private static void handleEvent(Consumer<String> consumer){
+        consumer.accept(name);
     }
 }
